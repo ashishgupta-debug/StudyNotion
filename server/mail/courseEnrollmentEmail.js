80b63cc04be5b1d9dc3ad232217947a154bf3660
Ashish gupta <128177589+ashishgupta-debug@users.noreply.github.com>
2024-08-11 19:50:45 +0530

    Update courseEnrollmentEmail.js

--- server/mail/courseEnrollmentEmail.js
+++ server/mail/courseEnrollmentEmail.js
@@ -79,7 +79,7 @@ exports.courseEnrollmentEmail = (courseName, name) => {
               <a class="cta" href="https://study-notio.vercel.app/dashboard">Go to Dashboard</a>
           </div>
           <div class="support">If you have any questions or need assistance, please feel free to reach out to us at <a
-                  href="mailto:info@studynotion.com">info@studynotion.com</a>. We are here to help!</div>
+                  href="mailto:ashishgupta639380@gmail.com">ahishgupta639380@gmail.com</a>. We are here to help!</div>
       </div>
   </body>
   
